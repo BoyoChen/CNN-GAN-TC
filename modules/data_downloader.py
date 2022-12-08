7c1af00028ff8321c02fc29adcc83c1fa3b5fb34
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 15:28:25 +0000

    Adding tarfile member sanitization to extractall()

--- modules/data_downloader.py
+++ modules/data_downloader.py
@@ -23,7 +23,26 @@ def uncompress_file(data_folder, file_name):
         download_compressed_file(data_folder, file_name)
 
     with tarfile.open(compressed_file_path) as tar:
-        tar.extractall(path=data_folder)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=data_folder)
 
 
 def verify_data(data_folder):
